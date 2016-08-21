80f18bd2ca5cd68685467542e57da240840b7c5a
DESKTOP-CEME6U3\Lione <lionelzhangz@gmail.com>
2016-08-21 22:04:06 +0800

    [Added]初始提交

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    /// <summary>
+    /// 关卡等级
+    /// </summary>
+    public int gameLevel = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
